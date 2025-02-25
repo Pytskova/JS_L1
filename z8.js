744797e0f1b9413e3dcb6f10694b6bae83ccab9b
Pytkova <pytkovaolga5@gmail.com>
2025-02-25 22:01:27 +0300

    lr 1

--- z8.js
+++ z8.js
@@ -5,17 +5,10 @@ function expandedForm(num) {
     for (let i = 0; i < strNum.length; i++) {
         let digit = strNum[i];
         if (digit !== '0') {
-            let zeros = '0'.repeat(strNum.length - i - 1);
-            result.push(digit + zeros);
+                let zeros = '0'.repeat(strNum.length - i - 1);
+                result.push(digit + zeros);
+            }
         }
+    
+        return result.join(' + ');
     }
-
-    return result.join(' + ');
-}
-
-// Примеры
-console.log(expandedForm(12));     // "10 + 2"
-console.log(expandedForm(45));     // "40 + 5"
-console.log(expandedForm(70304));  // "70000 + 300 + 4"
-console.log(expandedForm(5607));   // "5000 + 600 + 7"
-console.log(expandedForm(9000));   // "9000"
