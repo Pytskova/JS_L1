b278f04f55f50540063f42fe0649c82f3b9b96f5
Pytkova <pytkovaolga5@gmail.com>
2025-02-25 22:09:46 +0300

    lr 1

--- z9.js
+++ z9.js
@@ -2,16 +2,10 @@ function solution(str) {
     let result = [];
 
     for (let i = 0; i < str.length; i += 2) {
-        let pair = str[i] + (str[i + 1] || '_'); // Если символа нет, добавляем "_"
+        let pair = str[i] + (str[i + 1] || '_');
         result.push(pair);
     }
 
     return result;
 }
 
-// Примеры
-console.log(solution("abc"));    // ["ab", "c_"]
-console.log(solution("abcdef")); // ["ab", "cd", "ef"]
-console.log(solution("a"));      // ["a_"]
-console.log(solution(""));       // []
-console.log(solution("hello!")); // ["he", "ll", "o!"]
